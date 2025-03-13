a666ee3dc58f277d5ec03544aa76e40162c5f9c2
Dheerajnaik04 <115970172+Dheerajnaik04@users.noreply.github.com>
2025-03-13 23:12:24 +0530

    Added Rectangle2 class with area calculation

---
+++ Rectangle2.java
@@ -0,0 +1,11 @@
+class Rectangle2{
+int area(int a,int b){
+int res=a*b;
+return res;
+}
+public static void main(String Args[]){
+Rectangle2 r=new Rectangle2();
+int x=r.area(10,10);
+System.out.println(x);
+}
+}
